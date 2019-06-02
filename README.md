89cb4a291bb5a2530aa21f3e73525f3339466cd3
ghostInSh3ll <15273152851@163.com>
2019-06-02 11:00:12 +0800

    0530
    try out in Lab Computer (Ubuntu with GPU)

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ torch version = 1.1.0
 |fittingRegression.py|关系拟合（回归）|
 |classification.py|区分类别，分类，（我认为就是离散的回归）|
 |basicOperation.py|快速搭建网络、保存、读取网络、批训练`dataloader`、优化器效果对比|
+|CNN.py|CNN在MNIST手写数据集上的使用|
--- basicOperation.py
+++ basicOperation.py
@@ -75,109 +75,132 @@ y = x.pow(2) + 0.2*torch.rand(x.size())  # noisy y data (tensor), shape=(100, 1)
 # restore_net()
 # restore_params()
 
-# 批训练
-import torch
-import torch.utils.data as Data
-torch.manual_seed(1)    # reproducible
-
-BATCH_SIZE = 5      # 批训练的数据个数
-
-x = torch.linspace(1, 10, 10)       # x data (torch tensor)
-y = torch.linspace(10, 1, 10)       # y data (torch tensor)
-
-# print("\nx ",x)
-# print("\ny ",y)
-
-
-# 先转换成 torch 能识别的 Dataset
-# torch_dataset = Data.TensorDataset(data_tensor=x, target_tensor=y)
-torch_dataset = Data.TensorDataset(x, y)
-print(torch_dataset)
-
-# 把 dataset 放入 DataLoader
-loader = Data.DataLoader(
-    dataset=torch_dataset,      # torch TensorDataset format
-    batch_size=BATCH_SIZE,      # mini batch size
-    shuffle=True,               # 要不要打乱数据 (打乱比较好)
-    num_workers=2,              # 多线程来读数据
-)
-
-for epoch in range(3):   # 训练所有!整套!数据 3 次
-    for step, (batch_x, batch_y) in enumerate(loader):  # 每一步 loader 释放一小批数据用来学习
-        # 假设这里就是你训练的地方...
-
-        # 打出来一些数据
-        print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
-              batch_x.numpy(), '| batch y: ', batch_y.numpy())
-
-
-# #优化器效果对比
+# # 批训练
+# # the code in this part is run in anther computer with GPU, python 3.6, pytorch verison 0.3.0
+# '''
+# Here is the result:
+# <torch.utils.data.dataset.TensorDataset object at 0x7f86e6b9aac8>
+# Epoch:  0 | Step:  0 | batch x:  [6. 7. 2. 3. 1.] | batch y:  [ 5.  4.  9.  8. 10.]
+# Epoch:  0 | Step:  1 | batch x:  [ 9. 10.  4.  8.  5.] | batch y:  [2. 1. 7. 3. 6.]
+# Epoch:  1 | Step:  0 | batch x:  [ 3.  4.  2.  9. 10.] | batch y:  [8. 7. 9. 2. 1.]
+# Epoch:  1 | Step:  1 | batch x:  [1. 7. 8. 5. 6.] | batch y:  [10.  4.  3.  6.  5.]
+# Epoch:  2 | Step:  0 | batch x:  [3. 9. 2. 6. 7.] | batch y:  [8. 2. 9. 5. 4.]
+# Epoch:  2 | Step:  1 | batch x:  [10.  4.  8.  1.  5.] | batch y:  [ 1.  7.  3. 10.  6.]
+# '''
 # import torch
 # import torch.utils.data as Data
-# import torch.nn.functional as F
-# import matplotlib.pyplot as plt
-
-
-# # 伪数据
 # torch.manual_seed(1)    # reproducible
 
-# LR = 0.01
-# BATCH_SIZE = 32
-# EPOCH = 12
+# BATCH_SIZE = 5      # 批训练的数据个数
 
-# # fake dataset
-# x = torch.unsqueeze(torch.linspace(-1, 1, 1000), dim=1)
-# y = x.pow(2) + 0.1*torch.normal(torch.zeros(*x.size()))
+# x = torch.linspace(1, 10, 10)       # x data (torch tensor)
+# y = torch.linspace(10, 1, 10)       # y data (torch tensor)
 
-# # # plot dataset
-# # plt.scatter(x.numpy(), y.numpy())
-# # plt.show()
+# # print("\nx ",x)
+# # print("\ny ",y)
 
-# # 使用上节内容提到的 data loader
+
+# # 先转换成 torch 能识别的 Dataset
+# # torch_dataset = Data.TensorDataset(data_tensor=x, target_tensor=y)
 # torch_dataset = Data.TensorDataset(x, y)
-# loader = Data.DataLoader(dataset=torch_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2,)
+# print(torch_dataset)
 
-# # 每个优化器优化
-# # 默认的 network 形式
-# class Net(torch.nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.hidden = torch.nn.Linear(1, 20)   # hidden layer
-#         self.predict = torch.nn.Linear(20, 1)   # output layer
+# # 把 dataset 放入 DataLoader
+# loader = Data.DataLoader(
+#     dataset=torch_dataset,      # torch TensorDataset format
+#     batch_size=BATCH_SIZE,      # mini batch size
+#     shuffle=True,               # 要不要打乱数据 (打乱比较好)
+#     num_workers=2,              # 多线程来读数据
+# )
 
-#     def forward(self, x):
-#         x = F.relu(self.hidden(x))      # activation function for hidden layer
-#         x = self.predict(x)             # linear output
-#         return x
+# for epoch in range(3):   # 训练所有!整套!数据 3 次
+#     for step, (batch_x, batch_y) in enumerate(loader):  # 每一步 loader 释放一小批数据用来学习
+#         # 假设这里就是你训练的地方...
 
-# # 为每个优化器创建一个 net
-# net_SGD         = Net()
-# net_Momentum    = Net()
-# net_RMSprop     = Net()
-# net_Adam        = Net()
-# nets = [net_SGD, net_Momentum, net_RMSprop, net_Adam]
+#         # 打出来一些数据
+#         print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
+#               batch_x.numpy(), '| batch y: ', batch_y.numpy())
 
-# # different optimizers
-# opt_SGD         = torch.optim.SGD(net_SGD.parameters(), lr=LR)
-# opt_Momentum    = torch.optim.SGD(net_Momentum.parameters(), lr=LR, momentum=0.8)
-# opt_RMSprop     = torch.optim.RMSprop(net_RMSprop.parameters(), lr=LR, alpha=0.9)
-# opt_Adam        = torch.optim.Adam(net_Adam.parameters(), lr=LR, betas=(0.9, 0.99))
-# optimizers = [opt_SGD, opt_Momentum, opt_RMSprop, opt_Adam]
 
-# loss_func = torch.nn.MSELoss()
-# losses_his = [[], [], [], []]   # 记录 training 时不同神经网络的 loss
+#优化器效果对比
+# the code in this part is run in anther computer with GPU, python 3.6, pytorch verison 0.3.0
 
-# # Training & Visualization 
-# for epoch in range(EPOCH):
-#     print('Epoch: ', epoch)
-#     for step, (b_x, b_y) in enumerate(loader):
+import torch
+import torch.utils.data as Data
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
-#         # 对每个优化器, 优化属于他的神经网络
-#         for net, opt, l_his in zip(nets, optimizers, losses_his):
-#             output = net(b_x)              # get output for every net
-#             loss = loss_func(output, b_y)  # compute loss for every net
-#             opt.zero_grad()                # clear gradients for next train
-#             loss.backward()                # backpropagation, compute gradients
-#             opt.step()                     # apply gradients
-#             l_his.append(loss.data.numpy())     # loss recoder
 
+# 伪数据
+torch.manual_seed(1)    # reproducible
+
+LR = 0.01
+BATCH_SIZE = 32
+EPOCH = 12
+
+# fake dataset
+x = torch.unsqueeze(torch.linspace(-1, 1, 1000), dim=1)
+y = x.pow(2) + 0.1*torch.normal(torch.zeros(*x.size()))
+
+# # plot dataset
+# plt.scatter(x.numpy(), y.numpy())
+# plt.show()
+
+# 使用上节内容提到的 data loader
+torch_dataset = Data.TensorDataset(x, y)
+loader = Data.DataLoader(dataset=torch_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2,)
+
+# 每个优化器优化
+# 默认的 network 形式
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(1, 20)   # hidden layer
+        self.predict = torch.nn.Linear(20, 1)   # output layer
+
+    def forward(self, x):
+        x = F.relu(self.hidden(x))      # activation function for hidden layer
+        x = self.predict(x)             # linear output
+        return x
+
+# 为每个优化器创建一个 net
+net_SGD         = Net()
+net_Momentum    = Net()
+net_RMSprop     = Net()
+net_Adam        = Net()
+nets = [net_SGD, net_Momentum, net_RMSprop, net_Adam]
+
+# different optimizers
+opt_SGD         = torch.optim.SGD(net_SGD.parameters(), lr=LR)
+opt_Momentum    = torch.optim.SGD(net_Momentum.parameters(), lr=LR, momentum=0.8)
+opt_RMSprop     = torch.optim.RMSprop(net_RMSprop.parameters(), lr=LR, alpha=0.9)
+opt_Adam        = torch.optim.Adam(net_Adam.parameters(), lr=LR, betas=(0.9, 0.99))
+optimizers = [opt_SGD, opt_Momentum, opt_RMSprop, opt_Adam]
+
+loss_func = torch.nn.MSELoss()
+losses_his = [[], [], [], []]   # 记录 training 时不同神经网络的 loss
+from torch.autograd import Variable
+# Training & Visualization 
+for epoch in range(EPOCH):
+    print('Epoch: ', epoch)
+    for step, (b_x, b_y) in enumerate(loader):
+    	b_x = Variable(b_x)
+		b_y = Variable(b_y)
+
+        # 对每个优化器, 优化属于他的神经网络
+        for net, opt, l_his in zip(nets, optimizers, losses_his):
+            output = net(b_x)              # get output for every net
+            loss = loss_func(output, b_y)  # compute loss for every net
+            opt.zero_grad()                # clear gradients for next train
+            loss.backward()                # backpropagation, compute gradients
+            opt.step()                     # apply gradients
+            l_his.append(loss.data.numpy())     # loss recoder
+
+labels = ['SGD', 'Momentum', 'RMSprop', 'Adam']
+for i, l_his in enumerate(losses_his):
+    plt.plot(l_his, label=labels[i])
+plt.legend(loc='best')
+plt.xlabel('Steps')
+plt.ylabel('Loss')
+plt.ylim((0, 0.2))
+plt.show()
